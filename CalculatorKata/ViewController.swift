25589f75a32d89f39a587537c96314a58a2d7d6b
Alexander Spirichev <a.spirichev@dodopizza.com>
2019-09-17 23:26:05 +0300

    Initial Commit

---
+++ CalculatorKata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorKata
+//
+//  Created by Alexander Spirichev on 17.09.2019.
+//  Copyright © 2019 Alexander Spirichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
