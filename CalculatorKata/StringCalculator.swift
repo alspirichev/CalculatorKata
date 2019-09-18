dc26698a17179cca26390b15300dfc2cb9ed5a78
Alexander Spirichev <a.spirichev@dodopizza.com>
2019-09-18 20:23:26 +0300

    Empty string return zero

---
+++ CalculatorKata/StringCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  StringCalculator.swift
+//  CalculatorKata
+//
+//  Created by Alexander Spirichev on 18.09.2019.
+//  Copyright © 2019 Alexander Spirichev. All rights reserved.
+//
+
+import Foundation
+
+public class StringCalculator {
+    func add(_ numbers: String) -> Int {
+        return 0
+    }
+}
