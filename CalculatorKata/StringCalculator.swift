1153350fc2dc6bb861d75dc2d8921f5bf49309bf
Alexander Spirichev <alspirichev@gmail.com>
2019-10-06 19:03:55 +0300

    Add test with new line after comma Refactoring code

--- CalculatorKata/StringCalculator.swift
+++ CalculatorKata/StringCalculator.swift
@@ -8,9 +8,16 @@
 
 import Foundation
 
+enum InputError: Error {
+    case invalidFormat
+}
+
 public class StringCalculator {
-    func add(_ numbers: String) -> Int {
+    func add(_ numbers: String) throws -> Int {
         guard !numbers.isEmpty else { return 0 }
+        guard !numbers.contains(",\n") else {
+            throw InputError.invalidFormat
+        }
         
         let digits = numbers.components(separatedBy: "\n")
             .flatMap { $0.components(separatedBy: ",") }
--- CalculatorKataTests/CalculatorKataTests.swift
+++ CalculatorKataTests/CalculatorKataTests.swift
@@ -13,84 +13,93 @@ class StringCalculatorTests: XCTestCase {
     
     let sut = StringCalculator()
 
-    func test_whenNumbersStringIsEmpty_returnZero() {
+    func test_whenNumbersStringIsEmpty_returnZero() throws {
         let numbers = ""
-        
-        let result = self.sut.add(numbers)
-        
+
+        let result = try self.sut.add(numbers)
+
         XCTAssertEqual(result, 0, "Result should be zero when 'numbers' is empty")
     }
-    
-    func test_whenNumberStringIsOne_thenReturnOne() {
+
+    func test_whenNumberStringIsOne_thenReturnOne() throws {
         let numbers = "1"
-        
-        let result = self.sut.add(numbers)
-        
+
+        let result = try self.sut.add(numbers)
+
         XCTAssertEqual(result, 1)
     }
-    
-    func test_whenNumberStringMoreThenOne_thenReturnInputDigit() {
+
+    func test_whenNumberStringMoreThenOne_thenReturnInputDigit() throws {
         let numbers = "3"
-        
-        let result = self.sut.add(numbers)
+
+        let result = try self.sut.add(numbers)
 
         XCTAssertEqual(result, 3)
     }
-    
-    func test_whenPassTwoNumbers_thenReturnTheirSum() {
+
+    func test_whenPassTwoNumbers_thenReturnTheirSum() throws {
         let numbers = "1,2"
-        
-        let result = self.sut.add(numbers)
-        
+
+        let result = try self.sut.add(numbers)
+
         XCTAssertEqual(3, result)
     }
-    
-    func test_whenPassAnotherTwoNumber_thenReturnTheirSum() {
+
+    func test_whenPassAnotherTwoNumber_thenReturnTheirSum() throws {
         let numbers = "3,4"
-        
-        let result = self.sut.add(numbers)
-        
+
+        let result = try self.sut.add(numbers)
+
         XCTAssertEqual(7, result)
     }
-    
-    func test_whenPassFourNumbers_thenReturnTheirSum() {
+
+    func test_whenPassFourNumbers_thenReturnTheirSum() throws {
         let numbers = "1,2,3,4"
-        
-        let result = self.sut.add(numbers)
-        
+
+        let result = try self.sut.add(numbers)
+
         XCTAssertEqual(10, result)
     }
-    
-    func test_whenPassTenNumbers_thenReturnTheirSum() {
+
+    func test_whenPassTenNumbers_thenReturnTheirSum() throws {
         let numbers = "1,2,3,4,5,6,7,8,9,10"
-        
-        let result = self.sut.add(numbers)
-        
+
+        let result = try self.sut.add(numbers)
+
         XCTAssertEqual(55, result)
     }
-    
-    func test_whenPassThreeNumberWithOneNewLine_thenReturnTheirSum() {
+
+    func test_whenPassThreeNumberWithOneNewLine_thenReturnTheirSum() throws {
         let numbers = "1\n2,3"
-        
-        let result = self.sut.add(numbers)
-        
+
+        let result = try self.sut.add(numbers)
+
         XCTAssertEqual(6, result)
-    }
-    
-    func test_whenPassThreeNumberWithTwoNewLine_thenReturnTheirSum() {
-        let numbers = "1\n2\n3"
-        
-        let result = self.sut.add(numbers)
-        
-        XCTAssertEqual(6, result)
-    }
-    
-    func test_whenPassNumberWithThreeNewLineAndTwoComma_thenReturnTheirSum() {
-        let numbers = "5\n,6\n,7\n8"
-        
-        let result = self.sut.add(numbers)
-        
-        XCTAssertEqual(26, result)
     }
 
+    func test_whenPassThreeNumberWithTwoNewLine_thenReturnTheirSum() throws {
+        let numbers = "1\n2\n3"
+
+        let result = try self.sut.add(numbers)
+
+        XCTAssertEqual(6, result)
+    }
+
+    func test_whenPassNumberWithThreeNewLineAndTwoComma_thenReturnTheirSum() throws {
+        let numbers = "5\n,6\n,7\n8"
+
+        let result = try self.sut.add(numbers)
+
+        XCTAssertEqual(26, result)
+    }
+    
+    func test_whenPassInNumbersNewLineAfterComma_thenRaiseException() {
+        let numbers = "1,\n"
+        
+        XCTAssertThrowsError(try self.sut.add(numbers)) { error in
+            let throwError = error as! InputError
+            XCTAssertEqual(throwError, InputError.invalidFormat)
+        }
+    }
+    
 }
